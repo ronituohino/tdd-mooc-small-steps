fc8bd26f499446de98af18655a7c2a525968ed4d
Roni Tuohino <rt@ronituohino.com>
2026-02-26 06:49:23 +0200

    fully remove Date

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -21,16 +21,13 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string) as any;
-    const temporal = date?.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate() as
-      | Temporal.PlainDate
-      | undefined;
-    const cost = calculateCost(age, type, temporal, baseCost);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
-  function parseDate(dateString: string | undefined): Date | undefined {
+  function parseDate(dateString: string | undefined): Temporal.PlainDate | undefined {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 

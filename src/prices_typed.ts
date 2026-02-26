aa90d4e49dc43f6143740645f34d9c554c22d7c2
Roni Tuohino <rt@ronituohino.com>
2026-02-26 08:50:01 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -80,8 +80,8 @@ function createApp(database: Database) {
     return reduction;
   }
 
-  function isMonday(date2: Temporal.PlainDate) {
-    return date2.dayOfWeek === 1;
+  function isMonday(date: Temporal.PlainDate) {
+    return date.dayOfWeek === 1;
   }
 
   function isHoliday(date2: Temporal.PlainDate) {

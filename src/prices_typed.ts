60deed9af03cab35ef3b55c66bd08ca91ca5546d
Roni Tuohino <rt@ronituohino.com>
2026-02-26 07:07:30 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -31,8 +31,7 @@ function createApp(database: Database) {
     }
   }
 
-  const parsePlainDate = (dateString: string | undefined) =>
-    dateString ? Temporal.PlainDate.from(dateString) : undefined;
+  function parsePlainDate(dateString: string | undefined) {}
 
   function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number) {
     if (type === "night") {
